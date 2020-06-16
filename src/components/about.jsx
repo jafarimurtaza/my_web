79d6c6d828f488d0f621b63f4d56034d9218b614
Praksis User <murtaza>
2020-06-16 16:09:48 +0300

    my personal website

---
+++ src/components/about.jsx
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-12">
+                <div
+                  className="row row-bottom-padded-sm animate-box"
+                  data-animate-effect="fadeInLeft"
+                >
+                  <div className="col-md-12">
+                    <div className="about-desc">
+                      <span className="heading-meta">About Me</span>
+                      <h2 className="colorlib-heading">Who Am I?</h2>
+                      <p>
+                       I'm Murtaza, I am  a computer programmer grad student from Social-Hackers-Ackademy in Athens greece, with full-stack web dvelopment as my major. 
+                        I love exploring new technologies
+                        and often amazed by the progress we as a human species
+                        have mad so far in the recent years.
+                      </p>
+                      <p>
+                        I have started reflecting my ideas and thougths through
+                        the medium of words recently so spelling and grammer
+                        mistaks are very often.You can write me back if you spot
+                        any and don't want to live anymore :P{" "}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="colorlib-about">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">
+                  Here are some of my expertise
+                </h2>
+              </div>
+            </div>
+            <div className="row row-pt-md">
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                  <span className="icon">
+                    <i className="icon-bulb" />
+                  </span>
+                  <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>
+                      I have experience building websites and chrome extentions
+                      using JavaScript,HTML,CSS,React, Node.js, Sql
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                  <span className="icon">
+                    <i className="icon-data" />
+                  </span>
+                  <div className="desc">
+                    <h3>Data Structures</h3>
+                    <p>
+                      As coming from the database background, I have good grasp
+                      over fundamental concepts of DBMS,and working node.js with
+                      mysql
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                  <span className="icon">
+                    <i className="icon-layers2" />
+                  </span>
+                  <div className="desc">
+                    <h3>Web Design</h3>
+                    <p>
+                      I am pursuing my internship with web design team, and
+                      studant and working with tools like VS code, power point a
+                      lot of online tools etc.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              {/* <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>Jenkins , Kubernetes , Docker </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div> */}
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
